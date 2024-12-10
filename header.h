af62d5c3040311ad0445ac090d970be8e26ad453
grzechupaszek <105735927+grzechupaszek@users.noreply.github.com>
2024-12-10 13:43:22 +0000

    metody do aktualizacji stanu magazynu i do wyboru platnosci

--- header.h
+++ header.h
@@ -19,12 +19,22 @@ class Klient{
     int wybierz_platnosc();
     Towar wybierz_towar();
 };
-
+int Klient::wybierz_platnosc(){
+    int n;
+    cout<<"Platnosc do wyboru 1) gotowka 2) karta\n";
+    cin>>n;
+    if(n=1){/*zostala wybrana platnosc gotowka*/}
+    else if (n=2) {/*karta*/}
+    else {/*no i po co testujesz...*/}
+}
 class Admin{
     int id_admina = NULL;
     void akt_stanu_magazynu();
 };
-
+void Admin::akt_stanu_magazynu(){
+    cout<</*wybieranie produktu z bazy danych*/<<endl;
+    //tutaj trzeba zapytania sql wrzucic
+}
 class Magazynier{
     int id_magazyniera = NULL;
     void odebranie_towaru(Towar[]);
